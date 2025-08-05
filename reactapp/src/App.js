76b838c0c8db399051e41229736969d7049081c3
MD <24x55a0512@srecnandyal.edu.in>
2025-08-05 16:36:35 +0530

    date:05-08-2025

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,14 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-// import { Component } from 'react';
-// import Counter from './counter';
+import { Component } from 'react';
+import Counter from './counter';
 import Conditionalrender from './conditionalrender';
 function App() {
   
   return (
     <>
-    {/* <Counter/> */}
-    <Conditionalrender/>
+    <Counter/>
+    {/* <Conditionalrender/> */}
     </>
   );
 }
--- reactapp/src/counter.js
+++ reactapp/src/counter.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { useState } from 'react';
 
-const Counter = () => {
+const Counter=()=> {
     const[count,setCount] = useState(0);
-    const increase =  ()=>{
+    const plus =  ()=>{
         setCount(count+1);
     };
-    const decrease = ()=>{
+    const minus=()=>{
+      if(count<=0){
+        alert("cannot go further than zero")
+      }
+      else{
         setCount(count-1);
+      }
     }
-    const reset = ()=>{
+    const reset=()=>{
         setCount(0);
     }
   return (
     <>
     <h1>This show the count value</h1>
     <p id='counter'>{count}</p>
-    <button onClick={increase}>+1</button>
-    <button onClick={decrease}>-1</button>
+    <button onClick={plus}>+1</button>
+    <button onClick={minus}>-1</button>
     <button onClick={reset}>Reset</button>
     </>
   )
